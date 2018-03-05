8f2164f0c14ce9f16d67185b313ace87030e5edb
tomargit <34710137+tomargit@users.noreply.github.com>
2018-03-05 18:11:56 +0530

    First commit

---
+++ src/main/java/com/aonjob/gcj/App.java
@@ -0,0 +1,13 @@
+package com.aonjob.gcj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
